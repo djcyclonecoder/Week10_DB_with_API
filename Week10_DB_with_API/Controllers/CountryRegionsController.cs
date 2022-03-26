13a15ec98bd3ac66171b91d92fe2fc1914460f0d
djcyclonecoder <djcyclonecoder@gmail.com>
2022-03-26 13:28:55 -0500

    Add project files.

---
+++ Week10_DB_with_API/Controllers/CountryRegionsController.cs
@@ -0,0 +1,123 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Week10_DB_with_API.Data;
+using Week10_DB_with_API.Models;
+
+namespace Week10_DB_with_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryRegionsController : ControllerBase
+    {
+        private readonly Adventureworks2017Context _context;
+
+        public CountryRegionsController(Adventureworks2017Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CountryRegions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CountryRegion>>> GetCountryRegions()
+        {
+            return await _context.CountryRegions.ToListAsync();
+        }
+
+        // GET: api/CountryRegions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CountryRegion>> GetCountryRegion(string id)
+        {
+            var countryRegion = await _context.CountryRegions.FindAsync(id);
+
+            if (countryRegion == null)
+            {
+                return NotFound();
+            }
+
+            return countryRegion;
+        }
+
+        // PUT: api/CountryRegions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountryRegion(string id, CountryRegion countryRegion)
+        {
+            if (id != countryRegion.CountryRegionCode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(countryRegion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryRegionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CountryRegions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CountryRegion>> PostCountryRegion(CountryRegion countryRegion)
+        {
+            _context.CountryRegions.Add(countryRegion);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CountryRegionExists(countryRegion.CountryRegionCode))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCountryRegion", new { id = countryRegion.CountryRegionCode }, countryRegion);
+        }
+
+        // DELETE: api/CountryRegions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountryRegion(string id)
+        {
+            var countryRegion = await _context.CountryRegions.FindAsync(id);
+            if (countryRegion == null)
+            {
+                return NotFound();
+            }
+
+            _context.CountryRegions.Remove(countryRegion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryRegionExists(string id)
+        {
+            return _context.CountryRegions.Any(e => e.CountryRegionCode == id);
+        }
+    }
+}
